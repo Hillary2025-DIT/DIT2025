7d9e2273b527a7d4e03c1cfd7838410cbd356aa2
Hillary2025-DIT <alithumhillary337@gmail.com>
2025-11-24 11:52:06 +0300

    registration form

---
+++ form/form.js
@@ -0,0 +1,6 @@
+document.getElementById("form1").addEventListener("submit",function(event){
+    event.preventDefault()});
+    const formData=new FormData(this);
+    for(const[name,value] of formData.entries()){
+        console.log(`${name}:${value}`)
+    }
